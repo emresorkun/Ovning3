5497add5d00ed3902b9ea6aad280371cd8193ad9
Emre Sorkun <emresorkun@gmail.com>
2022-11-09 05:37:45 +0100

    age field and related condition created

---
+++ Ovning3/Person.cs
@@ -0,0 +1,49 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning3
+{
+    class Person
+    {
+        private int age;
+        public int Age
+        {
+            get => age;
+            set {
+                if (age>0)
+                {
+                    age=value;
+                }
+            }
+        }
+
+
+        private string fname;  
+        private string lname;
+        private int height;
+        private int weight;
+
+        public string SetName()
+        {
+            //fname is obligatory and at least 2 and no more then 10
+            return fname;  
+        }
+
+        //public string SetAge()
+        //{
+        //    //age should be more then 0
+        //    return age;
+        //}
+
+        //public string lname()
+        //{
+        //    //lname is obligatory and at least 3 and no more then 15
+        //    return lname;
+        //}
+
+        
+    }
+}
