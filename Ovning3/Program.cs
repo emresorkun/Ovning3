eea2bbd6082f2185097d223ab5077a82b40cf0e7
Emre Sorkun <emresorkun@gmail.com>
2022-11-11 09:26:13 +0100

    UserError Class created

--- Ovning3/Program.cs
+++ Ovning3/Program.cs
@@ -11,16 +11,18 @@
             //Ask user for inputs - Not nessasary
 
             var ph = new PersonHandler();
-            Person person1 = ph.CreatePerson(23, "Kalle", "Anka", 23, 70);
+            //Person person1 = ph.CreatePerson(23, "Kalle", "Anka", 23, 70);
+
+            Person person2 = ph.CreatePerson(43, "emre", "sorkun", 43,80);
 
             try
             {
                // var person1 = new Person();
                 //person1.Age = -3;
-                ph.SetAge(person1, 1);
-                ph.SetFirstname(person1, "default");
-                ph.SetLastname(person1, "default");
-                ph.SetHeight(person1, 12.12);
+                ph.SetAge(person2, 1);
+                ph.SetFirstname(person2, "default");
+                ph.SetLastname(person2, "default");
+                ph.SetHeight(person2, 12.12);
             }
             catch (ArgumentException e)
             {
